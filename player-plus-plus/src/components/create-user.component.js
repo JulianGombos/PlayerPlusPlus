ceded63b59b3c7dab1ae078de2a449408f5550bf
JulianGombos <julian.gombos@gmail.com>
2020-03-22 00:52:28 -0400

    Update create-user.component.js

--- player-plus-plus/src/components/create-user.component.js
+++ player-plus-plus/src/components/create-user.component.js
@@ -29,8 +29,14 @@ export default class CreateUser extends Component {
     console.log(user);
 
     axios.post('http://localhost:5000/users/add', user)
-      .then(res => console.log(res.data))
-      .catch(error => {console.log(error.response)});
+      .then(response => {
+        console.log(response.data)
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+      })
+      .catch(error => { console.log(error.response) });
 
     this.setState({
       username: ''
@@ -42,14 +48,14 @@ export default class CreateUser extends Component {
       <div>
         <h3>Create New User</h3>
         <form onSubmit={this.onSubmit}>
-          <div className="form-group"> 
+          <div className="form-group">
             <label>Username: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={this.state.username}
-                onChange={this.onChangeUsername}
-                />
+            <input type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
           </div>
           <div className="form-group">
             <input type="submit" value="Create User" className="btn btn-primary" />
